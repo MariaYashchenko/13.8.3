726af5e84e4e04b8d41b57f0f6f7146052605c37
MariaYashchenko <106230590+MariaYashchenko@users.noreply.github.com>
2022-07-18 12:27:52 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+tikets = int(input("Введите количество билетов:"))
+first = tikets [0]
+count = 0
+result =  ''
+for i in range tikets
+    age (i) = int(input("Введите свой возраст:"))
+        i + 1 = 1
+        if age <= 18:
+            print("Вход бесплатный")
+        elif age >=19 and <=25:
+            print ("Стоимость 990 рублей")
+        else age >=25:
+            print ("Стоимость билета 1390 рублей")
+if ticket_number > 3:
+    price_all = price_all - ((price_all / 100) * 10)
+    print(f"Сумма к оплате {price_all} руб. с учетом 10% скидки")
+    print(f'Сумма к оплате {price_all} руб.')
